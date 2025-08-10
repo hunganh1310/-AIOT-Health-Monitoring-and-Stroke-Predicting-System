22aad7514da4ce02be9111f4ec095b32cedcb9ba
hunganh1310 <hunganh1310.work@gmail.com>
2025-08-11 06:51:55 +0700

    Random Forest Done

--- crawl.py
+++ crawl.py
@@ -10,12 +10,7 @@ url = "https://zenodo.org/record/6807403/files/mimic_perform_non_af_csv.zip?down
 save_dir = "mimic_perform_non_af_csv"
 os.makedirs(save_dir, exist_ok=True)
 
-print("🔽 Đang tải dữ liệu từ Zenodo...")
 response = requests.get(url)
 if response.status_code == 200:
-    print("✅ Tải thành công, đang giải nén...")
     with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
         zip_ref.extractall(save_dir)
-    print(f"🎉 Giải nén xong! Dữ liệu nằm trong thư mục: {save_dir}")
-else:
-    print(f"❌ Lỗi khi tải dữ liệu: {response.status_code}")
